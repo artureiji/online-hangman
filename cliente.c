6def5eaab896e98a3401b64b236c87f77c4b87b1
Artur Eiji <artur.eiji@gmail.com>
2018-12-01 12:06:22 -0200

    Prepara cliente

--- cliente.c
+++ cliente.c
@@ -7,11 +7,16 @@ void printIntro() {
   printf("1) Iniciar partida simples\n");
   printf("2) Ser carrasco ao iniciar partida\n");
   printf("3) jogar no modo multiplayer\n");
+  printf("4) sair.\n");
+}
+
+void play_multiplayer() {
+
 }
 
 int main(int argc, char **argv) {
-    int connfd;
-    char error[300], choice;
+    int connfd, option;
+    char error[300], command[100];
     struct sockaddr_in servaddr;
 
     if (argc != 2) {
@@ -24,14 +29,23 @@ int main(int argc, char **argv) {
 
     connfd = Socket(AF_INET, SOCK_STREAM, 0);
 
-    servaddr = ClientSockaddrIn(AF_INET, argv[1], 10201);
+    servaddr = ClientSockaddrIn(AF_INET, argv[1], 9100);
 
     Connect(connfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+        
+    option = '0';
+    while (option != '4') {
+        printIntro();
+        printf("Digite sua escolha:");
+        option = getchar();
 
-    printIntro();
-
-    printf("Digite sua escolha:");
-    choice = getchar();
+        switch (option) {
+            case '3':
+                send(connfd, "Hello world!", 12, NULL);
+                play_multiplayer();
+                break;
+        }
+    }
 
     return 0;
 }
